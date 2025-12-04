0a02c27dc3a47321d880c2bd3f3297030c374cee
vlajksanyee <vlajksanyi@gmail.com>
2025-12-04 10:01:40 +0100

    chore: init

---
+++ src/main/java/com/vlajksanyi/animetracker/AnimetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.vlajksanyi.animetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimetrackerApplication.class, args);
+	}
+
+}
